79d86ee068aaa892b4a410be30ec2eea1b52707a
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-09-02 12:14:27 -0300

    primeiros codigos

---
+++ engenharia-software/Patio/src/Modelo.java
@@ -0,0 +1,38 @@
+
+public class Modelo {
+
+	private String modelo;
+	private String cor;
+	private String pais;
+	
+		
+	public Modelo(String modelo, String cor, String pais) {
+		super();
+		this.modelo = modelo;
+		this.cor = cor;
+		this.pais = pais;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public boolean comparar (Modelo ,modelo) {
+		if (modelo.equals(modelo))
+	}
+	
+
+}
