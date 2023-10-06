6a95647d41f18712a7acb05bf3bee4be000a492b
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-10-06 19:35:56 -0300

    teste

---
+++ engenharia-software/Patio/scr/testeTest.java
@@ -0,0 +1,17 @@
+package default;
+import org.junit.Test;
+import org.junit.Before;    
+    
+public class testeTest {
+
+    @Before
+    public void setup(){
+
+    }
+        
+    @Test
+    public void test() {
+        
+    }
+}
+    
